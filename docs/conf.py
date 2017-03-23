0bc0375f09d6ceb47c35b819457c4ff921bfe504
Yakov Pechersky <ffxen158@gmail.com>
2017-03-22 20:11:57 -0400

    use alabaster style nav sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,18 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+  'fixed_sidebar': True,
+}
+html_sidebars = {
+  '**': [
+    'about.html',
+    'navigation.html',
+    'relations.html',
+    'searchbox.html',
+    'donate.html',
+  ]
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
